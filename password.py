0421221a8e4915205ad9d7a7d536eaf2ab5e7de5
Daniel Chiu <lapulato@gmail.com>
2022-11-24 14:53:20 +0800

    move fail-- to upper loop

--- password.py
+++ password.py
@@ -1,13 +1,14 @@
 PASSWORD = 'abc123'
 fail = 3
 while fail > 0:
+	fail = fail - 1
 	password = input('input password:  ')
 	if password == PASSWORD:
 		print('登入成功')
 		break
 	else:
-		fail = fail - 1
 		if fail != 0:
 			print(f"wrong answer, 還有{fail}次機會")
-
+		else:
+			print(f"wrong answer, good bye")
 
